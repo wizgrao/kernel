1eb3b580585adb8cdc0c4c94768ed0371fb24101
Gaurav Rao <gaurarao@amazon.com>
2019-08-09 16:55:25 -0700

    some initial work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function point(x, y) {
+    return {x: x, y: y};
+}
+
+function drawList(list, tl, br, width, height) {
+    const subbed = sub(br, tl);
+    for (let i = 1; i < list.length; i++) {
+      const v1 =  sub(list[i-1], tl);
+      const v2 = sub(list[i], tl);
+      const x1 = v1.x / subbed.x * width;
+      const y1 = v1.y / subbed.y * height;
+      const x2 = v2.x / subbed.x * width;
+      const y2 = v2.y / subbed.y * height;
+      ctx.beginPath();
+      ctx.moveTo(x1,y1);
+      ctx.lineTo(x2, y2);
+      ctx.stroke();
+      ctx.closePath();
+    }
+}
+
+function getDir(angle, delta) {
+    return point(delta*math.cos(angle), delta*math.sin(angle));
+
+function next(points, step, n, dist) {
+    const current = points[points.length -1];
+    const prev = points[points.length -2];
+    let max = -1;
+    let maxPt = point(0,0);
+    for (let i = 0; i < n, i++) {
+        const pt = getStep(current, step, getDir(2*Math.PI*i/n, step), dist);
+        const d = dist(pt, prev);
+        if (d > max) {
+            max = d;
+            maxPt = pt;
+        }
+    }
+    return maxPt;
+}
+
+function eucNorm(pt) {
+    return Math.sqrt(pt.x*pt.x + pt.y*pt.y);
+}
+
+function scale(pt, s) {
+    return point(pt.x*s, pt.y*s);
+}
+
+function add(a, b) {
+    return point(a.x + b.x, a.y + b.y);
+}
+
+function sub(a, b) {
+    return point(a.x - b.x, a.y - b.y);
+}
+
+function getStep(p, delta, dir, dist) {
+    const dirNorm = scale(dir, delta/dist(p, add(p, dir)));
+    const metricDist = dist(p, add(dirNorm));
+    return add(p, scale(dirNorm, delta/metricDist));
+}
+
 function App() {
+  const canvasRef = useRef(null);
+  useEffect(() => {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      ctx.beginPath();
+      ctx.moveTo(0,0);
+      ctx.lineTo(200, 200);
+      ctx.stroke();
+      ctx.closePath();
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <canvas width={500} height={500} ref={canvasRef} />
     </div>
   );
 }
